9345791e6361c904cd044851fdf20db5409f0ebc
TheLegendVilva <202151195@iiitvadodara.ac.in>
2024-05-12 00:14:31 +0530

    pro model endpoint

---
+++ backend/resources/proText.js
@@ -0,0 +1,23 @@
+var express = require('express');
+require('dotenv').config();
+var get_text = require('../middleware/proTextMiddleware');
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+var router = express.Router();
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+
+
+router.post("/", async (req, res) => {
+    try {
+        query = req.body.text;
+        console.log(query);
+        const result = await get_text(query);
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+module.exports = router;
