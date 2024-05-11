b03998ecffc5dce5b5c708cde2da10a860adea91
TheLegendVilva <202151195@iiitvadodara.ac.in>
2024-05-12 01:44:31 +0530

    Implemented CORS

--- backend/resources/proText.js
+++ backend/resources/proText.js
@@ -1,14 +1,16 @@
 var express = require('express');
 require('dotenv').config();
 var get_text = require('../middleware/proTextMiddleware');
+var cors = require('cors');
 
+var corsOptions = require('../middleware/cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 var router = express.Router();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 
 
-router.post("/", async (req, res) => {
+router.post("/", cors(corsOptions),async (req, res) => {
     try {
         query = req.body.text;
         const result = await get_text(query);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+var corsOptions = require('../middleware/cors');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ msg: "Hello World from ExpressJS!"});
+router.get('/',cors(corsOptions),function(req, res, next) {
+  try{
+    res.json({ msg: "Hello World from ExpressJS!"});
+  }catch(err){
+    console.log(err);
+  }
 });
 
+// router.use(require('../errorHandler').errorForwarder);
+// router.use(require('../errorHandler').errorHandler);
 module.exports = router;
