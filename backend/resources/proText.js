8043898ad870d90c6444b92ee18d10985f25a424
nailsonseat <aadarshv3@gmail.com>
2024-05-12 11:47:33 +0530

    Cors hotfix

--- backend/resources/proText.js
+++ backend/resources/proText.js
@@ -1,25 +1,21 @@
-var express = require('express');
-require('dotenv').config();
-var get_text = require('../middleware/proTextMiddleware');
-var cors = require('cors');
+var express = require("express");
+require("dotenv").config();
+var get_text = require("../middleware/proTextMiddleware");
+var corsMiddleware = require("../middleware/cors");
 
-var corsOptions = require('../middleware/cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 var router = express.Router();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
-
-
-router.post("/", cors(corsOptions),async (req, res) => {
-    try {
-        query = req.body.text;
-        const result = await get_text(query);
-        res.json(result);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`Internal Server Error: ${error}`);
-    }
+router.post("/", corsMiddleware, async (req, res) => {
+  try {
+    query = req.body.text;
+    const result = await get_text(query);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Internal Server Error: ${error}`);
+  }
 });
-
 
 module.exports = router;
