23c0aca572d94983d57b47275d2ba5a8bb70670f
nailsonseat <aadarshv3@gmail.com>
2024-05-13 21:25:57 +0530

    Removed logging origin

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 var whitelist = [process.env.FRONTEND_DOMAIN];
 var corsOptions = {
   origin: function (origin, callback) {
-    console.log(origin);
-    console.log(process.env.FRONTEND_DOMAIN);
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
