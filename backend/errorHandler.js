4cbfdca8a7640b93b17933e6977a1ab3f0e3230b
nailsonseat <aadarshv3@gmail.com>
2024-05-13 20:21:16 +0530

    cors fixex and error message

--- backend/errorHandler.js
+++ backend/errorHandler.js
@@ -1,16 +1,15 @@
 function errorForwarder(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 }
 
 function errorHandler(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500).json({ error: err.message });
 }
 
-exports.errorForwarder = errorForwarder;    
+exports.errorForwarder = errorForwarder;
 exports.errorHandler = errorHandler;
