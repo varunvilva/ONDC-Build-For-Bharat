62d8d2c63e11f6d760b9ab3dea1fd4c8ee20a89d
nailsonseat <aadarshv3@gmail.com>
2024-05-13 20:40:57 +0530

    logging origin

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 var whitelist = [process.env.FRONTEND_DOMAIN];
 var corsOptions = {
   origin: function (origin, callback) {
+    console.log(origin);
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
