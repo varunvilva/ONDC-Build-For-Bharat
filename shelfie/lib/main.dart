15cd2612e9b66394a42fb74fac140bf23db5c8a7
nailsonseat <aadarshv3@gmail.com>
2024-05-14 14:11:43 +0530

    Color seed to blue accent

--- shelfie/lib/main.dart
+++ shelfie/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shelfie/routes.dart';
 import 'firebase_options.dart';
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'Shelfie',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
       routerConfig: routes,
