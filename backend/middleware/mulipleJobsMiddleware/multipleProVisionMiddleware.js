3135db9be5c91a6a355412775be98ee742ee5d34
TheLegendVilva <202151195@iiitvadodara.ac.in>
2024-05-16 10:28:10 +0530

    Handling Multiple images at once done

---
+++ backend/middleware/mulipleJobsMiddleware/multipleProVisionMiddleware.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+const get_multiple_image = async (image) => {
+  const image_prompt = `I am providing list of JSON attributes
+    product_name [String]
+    description [Text]
+    price [Float]
+    quantity [Float]
+    categories [list of String]
+    net_weight [Float]
+    barcode [String]
+    manufacturer_brand [String]
+    manufacturing_date [String] [DD-MM-YYYY]
+    expiration_date [String]  [DD-MM-YYYY]
+  
+    Find the attributes using the image given below, the attributes not found in the image should be filled with null value.
+    Do not fill quantity of stock as it will not be mentioned in the image.
+    Generate the categories and description based on product image
+    The dates should be in the format [DD-MM-YYYY] id found in the image
+    THE OUTPUT IS TO BE GIVEN IN JSON FORMAT
+    The user entered image is below :`;
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const imageParts = [
+    {
+      inlineData: {
+        data: image,
+        mimeType: "image/jpeg",
+      },
+    },
+  ];
+
+  const result = await model.generateContent([image_prompt, ...imageParts]);
+  const response = result.response;
+  const text = response.text();
+
+  const cleanTextOutput = text
+    .trim()
+    .replace(/^```/, "")
+    .replace(/```$/, "")
+    .replace(/json/gi, "");
+  const responseJson = JSON.parse(cleanTextOutput);
+  return responseJson;
+};
+
+module.exports = get_multiple_image;
