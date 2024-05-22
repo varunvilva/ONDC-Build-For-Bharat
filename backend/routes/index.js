5e5605b2c84f0933bed3def3e58edbd36e18bea0
nailsonseat <aadarshv3@gmail.com>
2024-05-22 23:07:23 +0530

    cors to index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,9 @@ var corsMiddleware = require("../middleware/cors");
 
 /* GET home page. */
 // router.options("/", corsMiddleware);
-router.get("/", function (req, res, next) {
+router.get("/", corsMiddleware, function (req, res, next) {
   try {
-    res.json({ msg: "Hello World Shelfie" });
+    res.json({ msg: "Hello World from ExpressJS!" });
   } catch (err) {
     console.log(err);
   }
